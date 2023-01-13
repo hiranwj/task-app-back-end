2c2f0998650d9c5d2ea1527150a5c5cc4b613727
hiranwj <hiranwj.connect@gmail.com>
2023-01-13 16:23:06 +0530

    Some changes to Secu Add apache common code dependency

--- src/main/java/lk/hiranwj/app/api/filter/SecurityFilter.java
+++ src/main/java/lk/hiranwj/app/api/filter/SecurityFilter.java
@@ -1,16 +1,49 @@
 package lk.hiranwj.app.api.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class SecurityFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Incoming Request...!");
+        if (req.getServletPath().matches("/api/v1/users") && req.getMethod().equals("POST")) {
+            chain.doFilter(req, res);
+        } else if (req.getServletPath().matches("/api/v1/auth/.+")) {
+            chain.doFilter(req, res);
+        } else {
+            String authorization = req.getHeader("Authorization");
+            if (authorization != null && authorization.startsWith("Basic")) {
+                String token = authorization.substring("Basic".length() + 1);
+                String credentials = new String(Base64.getDecoder().decode(token));
+                String username = credentials.split(":")[0];
+                String password = credentials.split(":")[1];
+                System.out.println(username + ": " + password);
+            }
+
+            Map<String, Object> errAttribute =  new LinkedHashMap<>();
+            errAttribute.put("status", HttpStatus.UNAUTHORIZED.value());
+            errAttribute.put("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            errAttribute.put("message", "Invalid login credentials...!");
+            errAttribute.put("timestamp", new Date().toString());
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            res.setContentType("application/json");
+            objectMapper.writeValue(res.getWriter(), errAttribute);
+
+            System.out.println("Incoming Request...!");
+        }
     }
 }
